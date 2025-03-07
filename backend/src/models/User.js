149fb9f0ee2f7aa1ac9728c8b4cc32334a869fba
karim.bisri <karim.bisri@sofrecom.com>
2025-03-07 21:38:02 +0000

    🐛 Correction du double hashage des mots de passe dans l'authentification

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -8,13 +8,5 @@ const UserSchema = new mongoose.Schema({
     role: { type: String, enum: ['user', 'admin'], default: 'user' }
 }, { timestamps: true });
 
-// Hashage du mot de passe avant sauvegarde
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
